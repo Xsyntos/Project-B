a93d7087e365e1d8493848e37a65d886579a6b25
Xsyntos <stevenschool2003@hotmail.com>
2021-03-01 14:36:19 +0100

    Initial commit

---
+++ ProjectRestaurant/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectRestaurant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!! World!");
+        }
+    }
+}
