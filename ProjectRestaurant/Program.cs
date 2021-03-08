3726859e186d6d77e7fe3802816355adfb5a080f
ArtywOw <Hamed.haydari.1998@gmail.com>
2021-03-08 15:22:39 +0100

    My project
    Restaurant

--- ProjectRestaurant/Program.cs
+++ ProjectRestaurant/Program.cs
@@ -6,7 +6,8 @@ namespace ProjectRestaurant
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!! World!");
+            string a = "Test";
+            Console.WriteLine(a);
         }
     }
 }
