c577d7bdce3aa39fedd7b8aefa02e75fe6b371d5
Yavuz Uca <yavuz_uca@hotmail.com>
2021-03-15 16:04:28 +0100

    Added a hash function to encrypt usernames and passwords.

---
+++ ProjectRestaurant/Hash.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+
+namespace ProjectRestaurant
+{
+    public class Hash
+    {
+        // Get function receives an input and returns a hash of the input.
+        public string Get(string text)
+        {
+            var bytes = Encoding.UTF8.GetBytes(text);
+            SHA256Managed hashstring = new SHA256Managed();
+            var hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (var x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
+        }
+    }
+}
