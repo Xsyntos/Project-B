e96c661d06a05349a3fb87c50422a36c5cf8faa4
Raysu <38209698+rayan-b@users.noreply.github.com>
2021-03-15 16:14:32 +0100

    Login Version 1

---
+++ ProjectRestaurant/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectRestaurant
+{
+    class Class1
+    {
+        public static void Login()
+        {
+            // login
+            Console.WriteLine("Please enter your username: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("Please enter your password: ");
+            string password = Console.ReadLine();
+
+            //Need JSON FILE to check
+            /*if (username == ... && password == ...) {
+                Console.WriteLine("Succesfuly logged in");
+            else
+                {
+                    Console.WriteLine("Wrong login/password. Try again");
+                }
+            */
+        }
+    }
+}
