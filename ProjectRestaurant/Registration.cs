b120f7c467b85547c31f73b35242d6ee5e1622ae
ArtywOw <Hamed.haydari.1998@gmail.com>
2021-03-15 17:03:51 +0100

    Menu and Registration
    Menu  and Registration of the application

---
+++ ProjectRestaurant/Registration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectRestaurant
+{
+    class Registration
+    {
+        public static void RegistrationFirstVersion()
+
+        {
+
+            /// Input Username
+            Console.WriteLine("Insert Username: ");
+            var username = Console.ReadLine();
+
+
+            {
+
+                /// Input Pasword 
+                Console.WriteLine("Insert Password: ");
+                var Password = Console.ReadLine();
+
+                /// Input Repeat Pasword 
+                Console.WriteLine("Confirm your password: ");
+                var repeat = Console.ReadLine();
+
+                /// Warnings 
+                if (Password.Length == repeat.Length && Password.Length > 7 && repeat.Length > 7)
+                {
+                    Console.WriteLine("Successfully registered");
+                }
+                else
+                {
+                    Console.WriteLine("Pasword or Username are incorrect, Please try again");
+                }
+
+            }
+
+
+        }
+    }
+}
