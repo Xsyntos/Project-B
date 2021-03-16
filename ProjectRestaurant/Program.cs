6f06704311148fc2ca9a919d691023c5f84189ef
Xsyntos <stevenschool2003@hotmail.com>
2021-03-16 14:42:31 +0100

    Json
    * Table regristration
    * User regristration
    * Reservation regristration

--- ProjectRestaurant/Program.cs
+++ ProjectRestaurant/Program.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 
+
+
 namespace ProjectRestaurant
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string a = "Test";
-            Console.WriteLine(a);
+            json_customer.customerinit();
+            json_table.tableInit();
+            json_reservation.reservationInit();
+         //   json_customer.newUser("Steven", "pass", "123");
+         //   json_reservation.makeReservation(new DateTime(2021, 12, 3, 12, 0, 0), json_customer.getUserlist()[0], json_table.getTableList()[0]);
+            json_customer.displayUser();
+            json_table.displayTable();
+            json_reservation.displayReservation();
+            // json_customer.displayUser();
         }
     }
 }
