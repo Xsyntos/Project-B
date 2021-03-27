291710e7992ab996d5e3712e983e91a058d275e7
Yavuz Uca <yavuz_uca@hotmail.com>
2021-03-27 15:56:45 +0100

    Check receives a string number and returns a string text

---
+++ ProjectRestaurant/Checker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectRestaurant
+{
+    class Checker
+    {
+        // Check receives a string number and returns a string text
+        public static string Check(string number)
+        {
+            string text = number.Length < 14 ? "Error: too short!" : number.Length > 14 ? "Error: too long!" : "Good enough!";
+            return text;
+        }
+    }
+}
