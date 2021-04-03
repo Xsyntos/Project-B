e48794baef30656da115e68d5b28dbf7e791da8a
ArtywOw <Hamed.haydari.1998@gmail.com>
2021-04-03 18:44:15 +0200

    FoodMenus
    First menu edited
    I add second menu which is Foodmenu2

--- ProjectRestaurant/foodmenu.cs
+++ ProjectRestaurant/foodmenu.cs
@@ -11,41 +11,53 @@ namespace ProjectRestaurant
             
                
                 {
+                double price = 0;
                 string Order = "Your Order is ";
-                    Console.WriteLine("Order a shit to eat");
-                    Console.WriteLine(@"1)Pasta Arabiata
- : shit shit shit");
-                    Console.WriteLine(@"2) Pasta coze 
- : shit shit shit");
-                    Console.WriteLine(@"3) Pasta shit
- : shit poop poop");
-                Console.WriteLine(@"4) Pasta damn
- : shit poop poop");
+                    Console.WriteLine("Please order your food here");
+                    Console.WriteLine(@"1)Pasta alla marinara
+ : Ham and spicy tomato sauce        Price: 9.50");
+                price = 9.50;
+                Console.WriteLine(@"2) Pasta salmone 
+ : Smoked salmon and cream sauce        Price: 10.50"  );
+                price = 10.50;
+                Console.WriteLine(@"3) Pasta vegetariana 
+ : Vegetables and tomato sauce        Price: 9.50");
+                price = 9.50;
+                Console.WriteLine(@"4) Pasta quattro formaggi
+ : 4 types of cheese and cream sauce        Price: 8.50");
+                price = 9.50;
                 ConsoleKeyInfo CostumerSelectaFood;
                     CostumerSelectaFood = Console.ReadKey(true);
 
 
                     switch (CostumerSelectaFood.KeyChar)
+                    
                     {
                         case '1':
                             Console.WriteLine(Order +  "Pasta Arabiata");
                             Console.ReadKey();
+                            
                             break;
                         case '2':
                             Console.WriteLine( Order + "Pasta coze ");
                             Console.ReadKey();
-                            break;
+                    
+                        break;
                     case '3':
                         Console.WriteLine(Order + "Pasta shit");
                         Console.ReadKey();
+             
                         break;
                     case '4':
                         Console.WriteLine(Order + "Pasta damn");
                         Console.ReadKey();
+               
                         break;
 
 
                 }
+               
+
 
             }
             
